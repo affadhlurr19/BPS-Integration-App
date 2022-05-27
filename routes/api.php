083109c5cf291894bccd3c42a3b4637f90da76f7
AlamAdhiyatama <itempunya139@gmail.com>
2022-05-28 00:33:11 +0700

    Menyelesaikan CRUD API dan Postman

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AbsensiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/absen', [AbsensiController::class, 'index']);
+Route::post('/absen/create', [AbsensiController::class, 'store']);
+Route::get('/absen/show/{id_absensi}', [AbsensiController::class, 'show']);
+Route::put('/absen/edit/{id_absensi}', [AbsensiController::class, 'update']);
+Route::delete('/absen/delete/{id_absensi}', [AbsensiController::class, 'destroy']);
